02a379a983c2bfcc98748348968fb904e9eb81c6
Harley de Brito <harleydebrito@gmail.com>
2020-10-26 17:03:49 -0300

    135 5:57

--- lib/tabs/home.tab.dart
+++ lib/tabs/home.tab.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomeTab extends StatelessWidget {
   @override
@@ -17,6 +21,43 @@ class HomeTab extends StatelessWidget {
                 title: const Text("Novidades"),
                 centerTitle: true,
               ),
+            ),
+            FutureBuilder<QuerySnapshot>(
+              future: _loadHome(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return SliverToBoxAdapter(
+                    child: Container(
+                      height: 200,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  );
+                } else {
+                  print(snapshot.data.docs[0].get("image"));
+                  return SliverStaggeredGrid.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 1.0,
+                    crossAxisSpacing: 1.0,
+                    staggeredTiles: snapshot.data.docs.map(
+                      (doc) {
+                        return StaggeredTile.count(doc.get("x"), doc.get("y"));
+                      },
+                    ).toList(),
+                    children: snapshot.data.docs.map(
+                      (doc) {
+                        return FadeInImage.memoryNetwork(
+                          placeholder: kTransparentImage,
+                          image: doc.get("image"),
+                          fit: BoxFit.cover,
+                        );
+                      },
+                    ).toList(),
+                  );
+                }
+              },
             ),
           ],
         ),
@@ -37,5 +78,10 @@ class HomeTab extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<QuerySnapshot> _loadHome() async {
+    await Firebase.initializeApp();
+    return FirebaseFirestore.instance.collection("home").orderBy("pos").get();
   }
 }
